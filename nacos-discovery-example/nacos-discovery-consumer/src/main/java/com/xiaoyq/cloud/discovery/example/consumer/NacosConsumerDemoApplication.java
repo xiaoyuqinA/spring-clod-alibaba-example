9184c128f460171d4fa4604353c3130ab538bc9a
肖玉琴 <xiaoyq01@mingyuanyun.com>
2021-08-29 21:28:58 +0800

    modify the Spring Cloud Version to be Spring Cloud 2020.0.1

--- nacos-discovery-example/nacos-discovery-consumer/src/main/java/com/xiaoyq/cloud/discovery/example/consumer/NacosConsumerDemoApplication.java
+++ nacos-discovery-example/nacos-discovery-consumer/src/main/java/com/xiaoyq/cloud/discovery/example/consumer/NacosConsumerDemoApplication.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,11 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@RestController
+@EnableDiscoveryClient
 public class NacosConsumerDemoApplication {
 
     @RestController
-    public class NacosController{
+    public static class NacosController{
 
         @Autowired
         private LoadBalancerClient loadBalancerClient;
--- nacos-discovery-example/nacos-discovery-provider/src/main/java/com/xiaoyq/cloud/discovery/example/provider/NacosProviderDemoApplication.java
+++ nacos-discovery-example/nacos-discovery-provider/src/main/java/com/xiaoyq/cloud/discovery/example/provider/NacosProviderDemoApplication.java
@@ -2,12 +2,13 @@ package com.xiaoyq.cloud.discovery.example.provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+@EnableDiscoveryClient
 public class NacosProviderDemoApplication {
 
     public static void main( String[] args ) {
@@ -15,7 +16,7 @@ public class NacosProviderDemoApplication {
     }
 
     @RestController
-    public class EchoController {
+    public static class EchoController {
         @GetMapping(value = "/echo/{string}")
         public String echo(@PathVariable String string) {
             return "Hello Nacos Discovery " + string;
